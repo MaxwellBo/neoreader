114ea066d0c127032ec0aea9c6cb08f68952ef80
Max Bo <max@maxbo.me>
2017-08-11 21:38:16 +1000

    Add support for saying and echoing

--- rplugin/python3/neoreader.py
+++ rplugin/python3/neoreader.py
@@ -1,10 +1,15 @@
 import neovim
+import subprocess
 
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.function('DoItPython')
-    def doItPython(self, args):
-        self.vim.command('echo "hello from DoItPython"')
+    @neovim.command('DoItPython')
+    def doItPython(self):
+        current = self.vim.current.line
+        self.vim.command(f'echo "{current}"')
+        subprocess.run(["say", current])
+        
+
